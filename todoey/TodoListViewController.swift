b211e4b5896eff3135c19bb74bab807cb764a072
SherifZekryWebsites <prosherifzekry@gmail.com>
2018-07-31 01:06:35 -0700

    To do list complete with no data presistence

---
+++ todoey/TodoListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  todoey
+//
+//  Created by Sherif  Wagih on 7/31/18.
+//  Copyright © 2018 Sherif  Wagih. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["First Item","Second Item","Third Item"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    //Mark - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count;
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItmeCell",for:indexPath);
+        cell.textLabel?.text = itemArray[indexPath.row];
+        return cell
+    }
+    //Mark - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true);
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        }
+        else
+        {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+    }
+}
+
