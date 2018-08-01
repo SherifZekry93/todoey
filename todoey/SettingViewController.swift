0e596d69bed6e29972349ee65f600ea72301f08f
SherifZekryWebsites <prosherifzekry@gmail.com>
2018-08-01 12:09:39 -0700

    add setting page

---
+++ todoey/SettingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingViewController.swift
+//  todoey
+//
+//  Created by Sherif  Wagih on 8/1/18.
+//  Copyright © 2018 Sherif  Wagih. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func backButtonPress(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
